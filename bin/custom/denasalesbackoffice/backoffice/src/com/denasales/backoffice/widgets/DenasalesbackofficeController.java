21afecd77b1b32359391e9223a6ecd52aa04e9bc
denasalesCorp <denasalescorp@gmail.com>
2018-03-10 11:15:58 +0200

    SetProduct created

--- bin/custom/denasalesbackoffice/backoffice/src/com/denasales/backoffice/widgets/DenasalesbackofficeController.java
+++ bin/custom/denasalesbackoffice/backoffice/src/com/denasales/backoffice/widgets/DenasalesbackofficeController.java
@@ -11,12 +11,9 @@
 package com.denasales.backoffice.widgets;
 
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Label;
 
 import com.hybris.cockpitng.util.DefaultWidgetController;
-
-import com.denasales.backoffice.services.DenasalesbackofficeService;
 
 
 public class DenasalesbackofficeController extends DefaultWidgetController
@@ -24,13 +21,9 @@ public class DenasalesbackofficeController extends DefaultWidgetController
 	private static final long serialVersionUID = 1L;
 	private Label label;
 
-	@WireVariable
-	private transient DenasalesbackofficeService denasalesbackofficeService;
-
 	@Override
 	public void initialize(final Component comp)
 	{
 		super.initialize(comp);
-		label.setValue(denasalesbackofficeService.getHello() + " DenasalesbackofficeController");
 	}
 }
